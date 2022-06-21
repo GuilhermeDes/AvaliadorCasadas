6a1795a6501dbef20b2aa486421e24628486b594
GuilhermeDes <59022920+GuilhermeDes@users.noreply.github.com>
2022-06-21 13:55:48 -0300

    implementação do codigo inicial

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("btncadastrar")
+document.getElementById("mainpop")
+window.document.alert('oi')
+function msgcadastrar() {
+    document.getElementById("mainpop").style.display = "flex"
+    document.alert('oi')
+}
